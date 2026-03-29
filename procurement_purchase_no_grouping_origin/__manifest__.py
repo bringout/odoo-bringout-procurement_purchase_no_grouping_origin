dc38a6a1117930eaa60483240b629a247a2f9160
Ernad Husremovic <hernad@bring.out.ba>
2026-03-29 12:05:01 +0200

    add summary field to __manifest__.py

--- procurement_purchase_no_grouping_origin/__manifest__.py
+++ procurement_purchase_no_grouping_origin/__manifest__.py
@@ -3,6 +3,7 @@
     "version": "16.0.1.0.0",
     "author": "bring.out doo Sarajevo",
     "website": "https://www.bring.out.ba",
+    "summary": "Group purchase orders by origin document so each MO generates a separate PO",
     "category": "Procurements",
     "depends": ["purchase_stock", 'procurement_purchase_no_grouping'],
     "data": [],
